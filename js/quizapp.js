3ac61124a530c40966f1a5ab589f122166a28698
Juan Soares Silva <juan07silva@gmail.com>
2021-11-21 19:15:11 -0300

    master

---
+++ js/quizapp.js
@@ -0,0 +1,43 @@
+const quizData = [
+    {
+        question:'How old is Florin?',
+        a:'10',
+        b:'17',
+        c:'26',
+        d:'110',
+        correct:'c'
+    }, 
+    {
+        question: 'What is the most used programming language in 2019?',
+        a:'Java',
+        b:'C',
+        c:'Python',
+        d:'JavaScript',
+        correct:'d'
+    },
+    {
+        question:'Who is the president of US?',
+        a:'Abraham Lincoln',
+        b:'Arnold Schwarzenneger',
+        c:'Joe Biden',
+        d:'Michael Schummaccher',
+        correct:'c'
+    },
+    {
+        question:'What does HTML stand for?',
+        a:'Hypertext Markup Language',
+        b:'Cascading Style Sheet',
+        c:'Jason Object Notation',
+        d:'Aplication Programming Interface',
+        correct:'a'
+    },
+    {
+        question:'What is the best movie of all the times?',
+        a:'Django',
+        b:'Interstellar',
+        c:'Titanic',
+        d:'Coco',
+        correct:'b'
+    }
+
+]

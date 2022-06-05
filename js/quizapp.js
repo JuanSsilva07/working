2c7260310a7ea866eed23c26868d6e0fe04172a0
Juan Soares Silva <juan07silva@gmail.com>
2022-06-05 14:43:37 -0300

    Mater

--- js/quizapp.js
+++ js/quizapp.js
@@ -1,43 +1,98 @@
-const quizData = [
+const quizData =[
     {
-        question:'How old is Florin?',
-        a:'10',
-        b:'17',
-        c:'26',
-        d:'110',
-        correct:'c'
-    }, 
-    {
-        question: 'What is the most used programming language in 2019?',
-        a:'Java',
-        b:'C',
-        c:'Python',
-        d:'JavaScript',
-        correct:'d'
+        question:"x",
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+        correct: "a"
     },
     {
-        question:'Who is the president of US?',
-        a:'Abraham Lincoln',
-        b:'Arnold Schwarzenneger',
-        c:'Joe Biden',
-        d:'Michael Schummaccher',
-        correct:'c'
+        question:"y",
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+        correct: "b"
     },
     {
-        question:'What does HTML stand for?',
-        a:'Hypertext Markup Language',
-        b:'Cascading Style Sheet',
-        c:'Jason Object Notation',
-        d:'Aplication Programming Interface',
-        correct:'a'
+        question:"w",
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+        correct: "c"
     },
     {
-        question:'What is the best movie of all the times?',
-        a:'Django',
-        b:'Interstellar',
-        c:'Titanic',
-        d:'Coco',
-        correct:'b'
-    }
+        question:"z",
+        a: "a",
+        b: "b",
+        c: "c",
+        d: "d",
+        correct: "d"
+    },
+];
 
-]
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+
+       currentQuiz++
+
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>Você respondeu ${score}/${quizData.length} questões corretamente</h2>
+
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
+    }
+})
